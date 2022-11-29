645d88c382c083e73a87fea6bfd42a887f3949b7
waylad <waylad42@gmail.com>
2022-11-29 14:08:29 +0100

    ABI function selector

--- src/NodeForm.tsx
+++ src/NodeForm.tsx
@@ -1,10 +1,11 @@
-import { Button, Form, Input } from "antd";
+import { Button, Form, Input, InputNumber, Select } from "antd";
 import React, { useContext, useState } from "react";
 import { BuilderContext, useDrawer } from "react-flow-builder";
 
 export const NodeForm: React.FC = (props: any) => {
   const { selectedNode: node } = useContext(BuilderContext);
-  const [abiCommands, setAbiCommands] = useState();
+  const [abi, setAbi] = useState<any>(undefined);
+  const [selectedCommand, setSelectedCommand] = useState<string>("");
 
   const { closeDrawer: cancel, saveDrawer: save } = useDrawer();
 
@@ -13,6 +14,7 @@ export const NodeForm: React.FC = (props: any) => {
   const handleSubmit = async () => {
     try {
       const values = await form.validateFields();
+      console.log("values", values);
       save?.(values);
     } catch (error) {
       const values = form.getFieldsValue();
@@ -20,297 +22,59 @@ export const NodeForm: React.FC = (props: any) => {
     }
   };
 
-  const handleAbi = (abi: string) => {
-    const parsedAbi = JSON.parse(abi);
-    console.log(parsedAbi);
-    const buttons = parsedAbi.functions?.map((func: any) => (
-      <div>
-        <br />
-        <h3>{func.name}</h3>
-        {func.inputs.map((inpt: any) => (
-          <div>
-            <label htmlFor="name">{inpt.name}</label>
-            <br />
-            <input type="text" id={inpt.name} name={inpt.name}></input>
-          </div>
-        ))}
-        <br />
-        <button>Generate command</button>
-      </div>
-    ));
-
-    setAbiCommands(buttons);
-
-    // {
-    //   "ABI version": 2,
-    //   "version": "2.2",
-    //   "header": ["pubkey", "time", "expire"],
-    //   "functions": [
-    //     {
-    //       "name": "constructor",
-    //       "inputs": [
-    //         {"name":"codeNft","type":"cell"},
-    //         {"name":"codeIndex","type":"cell"},
-    //         {"name":"codeIndexBasis","type":"cell"},
-    //         {"name":"ownerPubkey","type":"uint256"},
-    //         {"name":"json","type":"string"},
-    //         {"name":"mintingFee","type":"uint128"}
-    //       ],
-    //       "outputs": [
-    //       ]
-    //     },
-    //     {
-    //       "name": "mintNft",
-    //       "inputs": [
-    //         {"name":"json","type":"string"}
-    //       ],
-    //       "outputs": [
-    //       ]
-    //     },
-    //     {
-    //       "name": "withdraw",
-    //       "inputs": [
-    //         {"name":"dest","type":"address"},
-    //         {"name":"value","type":"uint128"}
-    //       ],
-    //       "outputs": [
-    //       ]
-    //     },
-    //     {
-    //       "name": "onTokenBurned",
-    //       "inputs": [
-    //         {"name":"id","type":"uint256"},
-    //         {"name":"owner","type":"address"},
-    //         {"name":"manager","type":"address"}
-    //       ],
-    //       "outputs": [
-    //       ]
-    //     },
-    //     {
-    //       "name": "setRemainOnNft",
-    //       "inputs": [
-    //         {"name":"remainOnNft","type":"uint128"}
-    //       ],
-    //       "outputs": [
-    //       ]
-    //     },
-    //     {
-    //       "name": "setMintingFee",
-    //       "inputs": [
-    //         {"name":"mintingFee","type":"uint128"}
-    //       ],
-    //       "outputs": [
-    //       ]
-    //     },
-    //     {
-    //       "name": "mintingFee",
-    //       "inputs": [
-    //         {"name":"answerId","type":"uint32"}
-    //       ],
-    //       "outputs": [
-    //         {"name":"value0","type":"uint128"}
-    //       ]
-    //     },
-    //     {
-    //       "name": "owner",
-    //       "inputs": [
-    //         {"name":"answerId","type":"uint32"}
-    //       ],
-    //       "outputs": [
-    //         {"name":"pubkey","type":"uint256"}
-    //       ]
-    //     },
-    //     {
-    //       "name": "transferOwnership",
-    //       "inputs": [
-    //         {"name":"newOwner","type":"uint256"}
-    //       ],
-    //       "outputs": [
-    //       ]
-    //     },
-    //     {
-    //       "name": "indexBasisCode",
-    //       "inputs": [
-    //         {"name":"answerId","type":"uint32"}
-    //       ],
-    //       "outputs": [
-    //         {"name":"code","type":"cell"}
-    //       ]
-    //     },
-    //     {
-    //       "name": "indexBasisCodeHash",
-    //       "inputs": [
-    //         {"name":"answerId","type":"uint32"}
-    //       ],
-    //       "outputs": [
-    //         {"name":"hash","type":"uint256"}
-    //       ]
-    //     },
-    //     {
-    //       "name": "resolveIndexBasis",
-    //       "inputs": [
-    //         {"name":"answerId","type":"uint32"}
-    //       ],
-    //       "outputs": [
-    //         {"name":"indexBasis","type":"address"}
-    //       ]
-    //     },
-    //     {
-    //       "name": "indexCode",
-    //       "inputs": [
-    //         {"name":"answerId","type":"uint32"}
-    //       ],
-    //       "outputs": [
-    //         {"name":"code","type":"cell"}
-    //       ]
-    //     },
-    //     {
-    //       "name": "indexCodeHash",
-    //       "inputs": [
-    //         {"name":"answerId","type":"uint32"}
-    //       ],
-    //       "outputs": [
-    //         {"name":"hash","type":"uint256"}
-    //       ]
-    //     },
-    //     {
-    //       "name": "getJson",
-    //       "inputs": [
-    //         {"name":"answerId","type":"uint32"}
-    //       ],
-    //       "outputs": [
-    //         {"name":"json","type":"string"}
-    //       ]
-    //     },
-    //     {
-    //       "name": "totalSupply",
-    //       "inputs": [
-    //         {"name":"answerId","type":"uint32"}
-    //       ],
-    //       "outputs": [
-    //         {"name":"count","type":"uint128"}
-    //       ]
-    //     },
-    //     {
-    //       "name": "nftCode",
-    //       "inputs": [
-    //         {"name":"answerId","type":"uint32"}
-    //       ],
-    //       "outputs": [
-    //         {"name":"code","type":"cell"}
-    //       ]
-    //     },
-    //     {
-    //       "name": "nftCodeHash",
-    //       "inputs": [
-    //         {"name":"answerId","type":"uint32"}
-    //       ],
-    //       "outputs": [
-    //         {"name":"codeHash","type":"uint256"}
-    //       ]
-    //     },
-    //     {
-    //       "name": "nftAddress",
-    //       "inputs": [
-    //         {"name":"answerId","type":"uint32"},
-    //         {"name":"id","type":"uint256"}
-    //       ],
-    //       "outputs": [
-    //         {"name":"nft","type":"address"}
-    //       ]
-    //     },
-    //     {
-    //       "name": "supportsInterface",
-    //       "inputs": [
-    //         {"name":"answerId","type":"uint32"},
-    //         {"name":"interfaceID","type":"uint32"}
-    //       ],
-    //       "outputs": [
-    //         {"name":"value0","type":"bool"}
-    //       ]
-    //     }
-    //   ],
-    //   "data": [
-    //   ],
-    //   "events": [
-    //     {
-    //       "name": "OwnershipTransferred",
-    //       "inputs": [
-    //         {"name":"oldOwner","type":"uint256"},
-    //         {"name":"newOwner","type":"uint256"}
-    //       ],
-    //       "outputs": [
-    //       ]
-    //     },
-    //     {
-    //       "name": "NftCreated",
-    //       "inputs": [
-    //         {"name":"id","type":"uint256"},
-    //         {"name":"nft","type":"address"},
-    //         {"name":"owner","type":"address"},
-    //         {"name":"manager","type":"address"},
-    //         {"name":"creator","type":"address"}
-    //       ],
-    //       "outputs": [
-    //       ]
-    //     },
-    //     {
-    //       "name": "NftBurned",
-    //       "inputs": [
-    //         {"name":"id","type":"uint256"},
-    //         {"name":"nft","type":"address"},
-    //         {"name":"owner","type":"address"},
-    //         {"name":"manager","type":"address"}
-    //       ],
-    //       "outputs": [
-    //       ]
-    //     }
-    //   ],
-    //   "fields": [
-    //     {"name":"_pubkey","type":"uint256"},
-    //     {"name":"_timestamp","type":"uint64"},
-    //     {"name":"_constructorFlag","type":"bool"},
-    //     {"name":"_supportedInterfaces","type":"optional(cell)"},
-    //     {"name":"_codeNft","type":"cell"},
-    //     {"name":"_totalSupply","type":"uint128"},
-    //     {"name":"_json","type":"string"},
-    //     {"name":"_codeIndex","type":"cell"},
-    //     {"name":"_codeIndexBasis","type":"cell"},
-    //     {"name":"_indexDeployValue","type":"uint128"},
-    //     {"name":"_indexDestroyValue","type":"uint128"},
-    //     {"name":"_deployIndexBasisValue","type":"uint128"},
-    //     {"name":"_owner","type":"uint256"},
-    //     {"name":"_remainOnNft","type":"uint128"},
-    //     {"name":"_lastTokenId","type":"uint128"},
-    //     {"name":"_mintingFee","type":"uint128"}
-    //   ]
-    // }
-  };
-
   return (
     <div>
       <Form form={form} initialValues={node?.data || { name: node?.name }}>
-        <Form.Item name="address" label="Address" rules={[{ required: true }]}>
+        <Form.Item name="name" label="Address" rules={[{ required: true }]}>
           <Input />
         </Form.Item>
-        <Form.Item name="name" label="Command" rules={[{ required: true }]}>
-          <Input disabled />
-        </Form.Item>
+
+        {abi ? (
+          <div>
+            <Form.Item
+              name="function"
+              label="Function"
+              rules={[{ required: true }]}
+            >
+              <Select
+                style={{ width: "100%" }}
+                onChange={(e: string) => setSelectedCommand(e)}
+              >
+                {abi.functions?.map((func: any) => (
+                  <Select.Option value={func.name}>{func.name}</Select.Option>
+                ))}
+              </Select>
+            </Form.Item>
+            <br />
+            {abi.functions
+              ?.filter((func: any) => func.name === selectedCommand)?.[0]
+              ?.inputs?.map((inpt: any) => (
+                <Form.Item
+                  name={inpt.name}
+                  label={inpt.name}
+                  rules={[{ required: true }]}
+                >
+                  <Input defaultValue="" placeholder={inpt.type} />
+                </Form.Item>
+              ))}
+          </div>
+        ) : (
+          <Form.Item name="abi" label="ABI" rules={[{ required: true }]}>
+            <Input.TextArea
+              rows={4}
+              placeholder="Paste ABI here..."
+              onChange={(e: any) => setAbi(JSON.parse(e.target.value))}
+            />
+          </Form.Item>
+        )}
       </Form>
+      <br />
       <div className="buttons-grid">
         <Button onClick={cancel}>Cancel</Button>
         <Button type="primary" onClick={handleSubmit}>
           Submit
         </Button>
       </div>
-
-      <h2>Generate Command</h2>
-      <Input.TextArea
-        rows={4}
-        placeholder="ABI"
-        onChange={(e: any) => handleAbi(e.target.value)}
-      />
-      {abiCommands}
     </div>
   );
 };
